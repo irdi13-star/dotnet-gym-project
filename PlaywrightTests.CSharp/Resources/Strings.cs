a5e04546a6af4640c5ca7da85c64c68e7c505b8d
IrinaDimitriu <176689948+irdi13-star@users.noreply.github.com>
2025-12-16 15:37:33 +0200

    fixed workflow

--- PlaywrightTests.CSharp/Resources/Strings.cs
+++ PlaywrightTests.CSharp/Resources/Strings.cs
@@ -6,7 +6,7 @@ public static class Strings
     {
         public const string HomeBrowserTitle = "Gym Management - Home";
         public const string HeroTitle = "Push Your Limits";
-        public const string HeroDescription = "Experience the best training programs tailored to your needszz.";
+        public const string HeroDescription = "Experience the best training programs tailored to your needs.";
     }
 
     public static class Auth
